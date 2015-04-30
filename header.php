4fc77d2469b4695a31e67a4be2e2c9e9a98421f2
Michael Foley <michaeldfoley@gmail.com>
2015-04-30 14:09:19 -0400

    added base styles and fonts

--- header.php
+++ header.php
@@ -31,10 +31,10 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
+		<nav id="site-navigation" class="main-navigation container" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Primary Menu', '_sbcgtheme' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+	<div id="content" class="container">
