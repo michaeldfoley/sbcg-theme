e4e446fdf45ac69ecc4f6d01cd8abe190ca1a7f4
Michael Foley <michaeldfoley@gmail.com>
2015-05-07 10:11:44 -0400

    removed comments from templates

--- content.php
+++ content.php
@@ -48,10 +48,7 @@
 			<?php endif; // End if $tags_list ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
 
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', '_sbcgtheme' ), __( '1 Comment', '_sbcgtheme' ), __( '% Comments', '_sbcgtheme' ) ); ?></span>
-		<?php endif; ?>
-
+		
 		<?php edit_post_link( __( 'Edit', '_sbcgtheme' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
