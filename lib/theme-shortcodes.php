4815bc5748e90eb839ada25aad0f44c80c4f8fe8
Michael Foley <michaeldfoley@gmail.com>
2015-06-02 17:01:41 -0400

    added link parameter to cards shortcode

--- lib/theme-shortcodes.php
+++ lib/theme-shortcodes.php
@@ -32,14 +32,18 @@ function cards_func( $atts, $content = '' ) {
   $args = shortcode_atts( array(
     'icon' => '',
     'title' => '',
+    'link' => '',
   ), $atts );
   
-  $out = sprintf( "\n<div class=\"card\">%s<div class=\"card-content\">%s%s</div></div>\n", 
+  $out = sprintf( "%s<div class=\"card-content\">%s%s</div>", 
           ($args['icon'] !== '') ? "<svg class=\"card-icon\" role=\"presentation\"><use xlink:href=\"" . get_stylesheet_directory_uri() . "/assets/images/sprite.symbol.svg#{$args['icon']}\" /></svg>" : "",
           ($args['title'] !== '') ? "<h4 class=\"card-title\">{$args['title']}</h4>" : "",
           $content
         );
-    
+  $out = sprintf( "\n<div class=\"card\">%s</div>\n",
+          ($args['link'] !== '') ? "<a href=\"{$args['link']}\">{$out}</a>" : $out
+        );
+            
   return $out;
 }
 add_shortcode( 'card', 'cards_func' );
